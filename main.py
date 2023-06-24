4c2d01d93a3d4c501f19925ec0ea977479815c1f
HarshavardhanGowda <harsha7697@gmail.com>
2023-06-24 16:50:47 +0530

    Functioning version that can be used by others

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 import json
-import logging
+import os
+import structlog
 
-def process_data(data):
-    output = []
-    for line in data.split(">>>"):
+logger = structlog.get_logger()
+
+
+def process_raw_log_data(raw_log_data: str) -> list:
+    """converts the raw log data to readable format.
+
+    Args:
+        raw_log_data (str): the raw log data.
+
+    Returns:
+        list: list of json objects.
+    """
+    logger.info('Processing raw log data')
+    formatted_log_data = []
+    for line in raw_log_data.split('>>>'):
         line = line.strip()
         if line:
             parsed_line = json.loads(line)
-            message = parsed_line.get("message", "")
-            timestamp = parsed_line.get("@timestamp", "")
-            timestamp = timestamp.replace("T", " ").replace("Z", "")
-            level = parsed_line.get("log", {}).get("level", "")
-            output.append(f"{timestamp} [{level}] {message}")
+            message = parsed_line.get('message', '')
+            timestamp = parsed_line.get('@timestamp', '')
+            timestamp = timestamp.replace('T', ' ').replace('Z', '')
+            level = parsed_line.get('log', {}).get('level', '')
+            formatted_log_data.append(f'{timestamp} [{level}] {message}')
+    return formatted_log_data
 
-    return output
 
 # Read data from file
-# filename = "agent.log.0"
-filename = str(input("Enter the absolute path of the log file: "))
-with open(filename, "r") as file:
+INPUT_FILENAME = str(input('Enter the absolute path of the log file: '))
+with open(INPUT_FILENAME, 'r', encoding='utf-8') as file:
+    logger.info('Reading data from file')
     data = file.read()
 
 # Process the data
-output = process_data(data)
+output = process_raw_log_data(data)
+output_filename = os.getcwd() + '/outputs/new_agent_log.log'
 
-with open("new_agent.log.0", "w") as file:
+with open(output_filename, 'w', encoding='utf-8') as file:
     for log_event in output:
-        file.write(log_event + "\n")
+        file.writelines(log_event + '\n')
 
-logging.critical("Successfully generated new_agent.log.0")
+logger.info('Successfully generated new_agent_log.log')
