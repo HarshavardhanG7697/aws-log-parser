f75c99e92e5771ba6643872d8d78e12f5d731621
HarshavardhanGowda <harsha7697@gmail.com>
2023-07-09 15:17:38 +0000

    removed unnecessary logging and reformatted the code.

--- aws-log-parser.py
+++ aws-log-parser.py
@@ -7,28 +7,21 @@ logger = structlog.get_logger()
 
 MIGRATION_SERVICES = ["ce", "drs", "mgn"]
 CE_LOG_DIR = "/var/lib/aws-replication-agent"
-CE_LOG_FILE = "/agent.log.0"
+CE_LOG_FILE = "agent.log.0"
 
 
-def log_dir_exists(service):
-    if service in MIGRATION_SERVICES:
-        return os.path.exists(CE_LOG_DIR)
-    else:
-        logger.debug(f"{CE_LOG_DIR} not exist")
-        return False
+def log_dir_exists(service) -> bool:
+    return service in MIGRATION_SERVICES and os.path.exists(CE_LOG_DIR)
 
 
-def log_file_exists(service):
-    if service in MIGRATION_SERVICES:
-        logger.debug(f"{os.getcwd() + CE_LOG_FILE} exist")
-        return os.path.exists(os.getcwd() + CE_LOG_FILE)
-    else:
-        logger.debug(f"{os.path.exists(os.getcwd() + CE_LOG_FILE)} not exist")
-        return False
+def log_file_in_current_dir(service) -> bool:
+    current_dir = os.getcwd()
+    return service in MIGRATION_SERVICES and os.path.exists(os.path.join(current_dir, CE_LOG_FILE))
 
 
 def parse_logs(raw_log_file):
     parsed_logs = []
+    logger.info("parsing log file.")
     with open(raw_log_file, "r") as raw_log_data:
         for line in raw_log_data:
             line = line.split('>>>')
@@ -55,18 +48,18 @@ def parse_logs(raw_log_file):
 @click.argument('service')
 def main(service="ce"):
     output = []
+    log_file = ""
+
     if log_dir_exists(service):
-        logger.warn(log_dir_exists(service))
-        log_file = CE_LOG_DIR + CE_LOG_FILE
-        logger.info(f"located {log_file}.")
-        output = parse_logs(log_file)
-    elif log_file_exists(service):
-        logger.warn(log_dir_exists(service))
-        log_file = os.getcwd() + CE_LOG_FILE
+        log_file = os.path.join(CE_LOG_DIR, CE_LOG_FILE)
+    elif log_file_in_current_dir(service):
+        log_file = os.path.join(os.getcwd(), CE_LOG_FILE)
+
+    if log_file:
         logger.info(f"located {log_file}.")
         output = parse_logs(log_file)
     else:
-        logger.critical(f"no raw logs found.")
+        logger.critical("no raw logs found.")
 
     click.echo_via_pager(output)
 
